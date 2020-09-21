70769204d7d32cccdd46d52cc5afd75d51639269
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2020-09-20 22:39:51 -0700

    added forecast api

--- README.md
+++ README.md
@@ -16,6 +16,9 @@ SO THAT I can plan a trip accordingly
 ```
 GIVEN a weather dashboard with form inputs
 WHEN I search for a city
+
+//create a form in HTML to enter in City
+
 THEN I am presented with current and future conditions for that city and that city is added to the search history
 WHEN I view current weather conditions for that city
 THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
--- script.js
+++ script.js
@@ -2,18 +2,42 @@
        
        
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=c56b8c5094d7dabc849248635865a867"
-var citySelection = []   
+var query5Day = "https://api.openweathermap.org/data/2.5/forecast?q=Seattle&appid=c56b8c5094d7dabc849248635865a867"
+var citySelection = []  
+
     
- {
+ 
    $.ajax({
    url: queryURL,
    method: "GET"
    }).then(function(response) {
-   console.log(response.weather[0].description);
+   console.log(response);
+   weatherOptions = [response.weather[0].description, response.main.temp, response.main.humidity, response.wind.speed, ]
+   var humidity = (response.main.humidity)
+   var windSpeed = (response.wind.speed)
+   var currentDescr = response.weather[0].description
+   var temp = (response.main.temp)
+   var temp = ((response.main.temp)*1.8 - 459.67)
+   console.log(temp);
+   });
+
+   $.ajax({
+    url: query5Day,
+    method: "GET"
+    }).then(function(response) {
+    // weather5Day = [response.main.temp, response.main.humidity ]
+    console.log(response);
+
     
-   })};
+    });
+   
+
+
+   // weather conditions .weather[0].description, temperature .main.temp, humidity .main.humidity, wind speed .wind.speed, UV index
 
     // ajaxCall()
-       
+    // var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    // console.log(temp);
+   
 
   
